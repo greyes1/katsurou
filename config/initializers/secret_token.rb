7aa251fbee62740bad99a1799d9c075f55af0abe
BIG HEAD <hdgajkhgjka>
2013-12-12 13:10:29 -0600

    added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Katsu::Application.config.secret_token = 'f5b96bbd6db5e419eaf187e3c71dcb59812bdd06da5b8fccc04911f6e5ae5fce89fc2054fb13baf3e77d5be3c1840bca371b0acd5c773bdfe3d2db4bc1945bc0'
